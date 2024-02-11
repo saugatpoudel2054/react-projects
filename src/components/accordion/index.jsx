3c911617a062b31fbb8f58fe41d9e0a82476c10d
Saugat Poudel <saugatpoudel2054@gmail.com>
2024-02-11 20:17:14 +0545

    Used multiselection enable and disable functionality

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -4,21 +4,33 @@ import './styles.css';
 
 const Accordion = () => {
     const [selected, setSelected] = useState([0,0,0]);
+    const [enableMultiSelect, setEnableMultiSelect] = useState(false);
+    const [singleSelectedItem, setSingleSelectedItem] = useState(null);
 
     const handleSelection = (getCurrentId) => {
         console.log(getCurrentId);
-        if(selected[getCurrentId] === 1){
-            let updatedSelected = [...selected];
-            updatedSelected[getCurrentId] = 0
-            setSelected(updatedSelected);
+        if(enableMultiSelect){
+            console.log('Multislection enabled');
+            if(selected[getCurrentId] === 1){
+                let updatedSelected = [...selected];
+                updatedSelected[getCurrentId] = 0
+                setSelected(updatedSelected);
+            }else{
+                let updatedSelected = [...selected];
+                updatedSelected[getCurrentId] = 1
+                setSelected(updatedSelected);
+            }
         }else{
-            let updatedSelected = [...selected];
-            updatedSelected[getCurrentId] = 1
-            setSelected(updatedSelected);
+            console.log('Multislection disabled');
+            setSingleSelectedItem(getCurrentId === singleSelectedItem ? null : getCurrentId);
         }
+        
     }
 
     return <div className="wrapper">
+        <button type="button" onClick={() => setEnableMultiSelect(!enableMultiSelect)}>
+            {enableMultiSelect ?  <span>Enable Single Selection</span> : <span>Enable Multi-selection</span>}
+        </button>
         <div className="accordion">
             {
                 data && data.length > 0 ? 
@@ -33,7 +45,12 @@ const Accordion = () => {
                     </div>
                     <div className="description">
                         {
+                            enableMultiSelect ?
                             selected[dataItem.id] === 1 ? 
+                            (<div>
+                                {dataItem.description}
+                            </div>) : null
+                            : singleSelectedItem === dataItem.id ? 
                             (<div>
                                 {dataItem.description}
                             </div>) : null
